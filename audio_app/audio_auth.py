deb4dd788bc9c903cb7d3593351667dd8d34887c
Kevin <la@owners-MacBook-Air.local>
2023-11-10 17:27:22 +0300

    update

---
+++ audio_app/audio_auth.py
@@ -0,0 +1,31 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/calendar']
+
+
+def authenticate():
+    """Authenticate the user and obtain valid credentials."""
+    credentials = None
+
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            print('Refreshing Access Token...')
+            credentials.refresh(Request())
+        else:
+            print('Fetching New Tokens...')
+            credentials = run_authentication_flow() 
+    return credentials.to_json()
+
+def run_authentication_flow():
+    """Run the OAuth 2.0 authentication flow to get new credentials."""
+    flow = InstalledAppFlow.from_client_secrets_file(
+        'cred.json',
+        scopes=SCOPES,
+        redirect_uri='https://mqbackend.myratecardinfo.com/auth/callback'
+    )
+
+    flow.run_local_server(port=8050, prompt='consent', authorization_prompt_message='')
+
+    return flow.credentials
