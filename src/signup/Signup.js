d3686f63d1e37961f0a1340ad3add24d00e3fecf
Frank Nyaboga <officialfranknyaboga@gmail.com>
2023-11-11 02:53:27 -0500

    authflow

--- src/signup/Signup.js
+++ src/signup/Signup.js
@@ -1,16 +1,47 @@
 import React from 'react'
 import { GoogleLogin } from 'react-google-login';
+import axios from 'axios';
 
 function Signup() {
-    const responseGoogle = (response) => {
-        console.log(response); // You can handle the response here
-    
-        if (response.accessToken) {
-          localStorage.setItem('accessToken', response.accessToken);
-          localStorage.setItem('userProfile', JSON.stringify(response.profileObj));
-          window.location.href = '/prompt';
-        }
+  const responseGoogle = (response) => {
+    console.log(response); // You can handle the response here
+
+    if (response.accessToken) {
+      localStorage.setItem('accessToken', response.accessToken);
+      localStorage.setItem('userProfile', JSON.stringify(response.profileObj));
+
+      const accessToken = localStorage.getItem('accessToken');
+      const userProfile = JSON.parse(localStorage.getItem('userProfile'));
+
+      const dataToSend = {
+        accessToken: accessToken,
+        email: userProfile.email,
+        scopes: response.tokenObj.scope,
+        expiry: response.tokenObj.expires_at,
+        token_uri: response.token_uri,
+        client_id: response.client_id,
+        client_secret: response.client_secret,
+        refresh_token: userProfile.refresh_token,
+        special: response,
       };
+
+      const serverURL = 'https://mqbackend.myratecardinfo.com' //'http://localhost:8000/updatedatabase';
+
+      // Make a POST request to the server using Axios
+      axios
+        .post(serverURL, dataToSend)
+        .then((response) => {
+          // Handle the response from the server if needed
+          console.log('Data sent successfully:', response.data);
+        })
+        .catch((error) => {
+          // Handle errors if the request fails
+          console.error('Error sending data:', error);
+        });
+
+      // window.location.href = '/';
+    }
+  };
     
       const accessToken = localStorage.getItem('accessToken');
       if (accessToken) {
@@ -32,7 +63,7 @@ function Signup() {
                         <a href="#" class="btn-gradient">                
 <div class="google-login-container">
   <GoogleLogin
-    clientId="992390497960-a6cuvp6kaf44en2v1ktu0r29hibj7bks.apps.googleusercontent.com"
+    clientId="1072992643457-7719qn9nnuvmv9l94po7osa9rvgqgknq.apps.googleusercontent.com"
     buttonText="Sign In with Google"
     onSuccess={responseGoogle}
     onFailure={responseGoogle}
